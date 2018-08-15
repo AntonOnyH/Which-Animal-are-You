c42ea67a6f1249c85651a4c4150878c1796271fb
Anton Huisamen <antonmcde@icloud.com>
2018-08-15 17:29:11 +0200

    The start

---
+++ Questionaire/Questionaire/QuestionViewController.swift
@@ -0,0 +1,20 @@
+//
+//  QuestionViewController.swift
+//  Questionaire
+//
+//  Created by Anton Huisamen on 2018/08/12.
+//  Copyright © 2018 antonsolo. All rights reserved.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+  
+
+}
---
+++ Questionaire/Questionaire/ResultsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ResultsViewController.swift
+//  Questionaire
+//
+//  Created by Anton Huisamen on 2018/08/12.
+//  Copyright © 2018 antonsolo. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+  
+
+}
