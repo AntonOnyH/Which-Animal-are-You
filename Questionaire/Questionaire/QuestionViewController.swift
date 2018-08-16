255ea0062c98159ab661dcfb7ed9db814c930bfc
Anton Huisamen <antonmcde@icloud.com>
2018-08-16 18:09:06 +0200

    nested arrays and structs

--- Questionaire/Questionaire/QuestionViewController.swift
+++ Questionaire/Questionaire/QuestionViewController.swift
@@ -9,12 +9,43 @@
 import UIKit
 
 class QuestionViewController: UIViewController {
-
+    
+    var questions: [Question] = [
+        Question (text: "Which of these do you enjoy most?",
+                  type: Responsetype.single,
+                  answers: [
+                           Answer(text: "steak", type: case.dog),
+                           Answer(text: "Fish", type: case.cat),
+                           Answer(text: "Carrots", type: case.rabbit),
+                           Answer(text: "Corn", type: case.turtle)
+    ]),
+        Question(text: "Which activities do you enjoy",
+                 type: Responsetype.multiple,
+                 answers: [
+                          Answer(text: "swimming", type: case.turtle),
+                          Answer(text: "Sleeping", type: case.cat),
+                          Answer(text: "Cuddling", type: case.rabbit),
+                          Answer(text: "Eating", type: case.dog)
+    ]),
+        Question(text: "How much do you enjoy car rides",
+                 type: Responsetype.ranged,
+                 answers: [
+                          Answer(text: "I dislike them", type: case.cat),
+                          Answer(text: "I get a little nervous", type: case.rabbit),
+                          Answer(text: "I barely notice them", type: case.turtle),
+                          Answer(text: "I love them", type: case.dog)
+    ])
+    ]
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
-
-  
-
+    
+    
+    
 }
--- Questionaire/Questionaire/Questiondata.swift
+++ Questionaire/Questionaire/Questiondata.swift
@@ -7,8 +7,9 @@
 //
 
 import Foundation
+import UIKit
 
-struct Quesion {
+struct Question {
     var text: String
     var type: Responsetype
     var answers: [Answer]
@@ -26,6 +27,19 @@ struct Answer {
 
 enum Animal: Character {
     case dog = "🐶",cat = "🐱",rabbit = "🐰", turtle = "🐢"
+    
+    var definition: String {
+        switch self {
+        case .dog:
+            return "You are incredibly outgoing. You surround yourself with your loved ones and enjoy activities with your friends."
+        case .cat:
+            return "Mischieves, yet mild tempered. You enjoy doing things on your own terms"
+        case .rabbit:
+            return "You love everything that's soft. You are healthy and full of energy"
+        case .turtle:
+            return "You are wise beyond your years, you focus on details. Slow and steady wins the race"
+        }
+    }
 }
 
 
