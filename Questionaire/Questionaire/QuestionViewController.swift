dc9e3919d78cb88b1c768d1de7159a145c4bd9f9
Anton Huisamen <antonmcde@icloud.com>
2018-08-20 14:21:12 +0200

    update label text

--- Questionaire/Questionaire/QuestionViewController.swift
+++ Questionaire/Questionaire/QuestionViewController.swift
@@ -10,13 +10,28 @@ import UIKit
 
 class QuestionViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    
     @IBOutlet weak var singleStackView: UIStackView!
     
+    @IBOutlet weak var singleButton1: UIButton!
+    @IBOutlet weak var singleButton2: UIButton!
+    @IBOutlet weak var singleButton3: UIButton!
+    @IBOutlet weak var singleButton4: UIButton!
+    
     @IBOutlet weak var multipleStackView: UIStackView!
+    @IBOutlet weak var multiLabel1: UILabel!
+    @IBOutlet weak var multiLabel2: UILabel!
+    @IBOutlet weak var multiLabel3: UILabel!
+    @IBOutlet weak var multiLabel4: UILabel!
     
     
     @IBOutlet weak var rangedStackView: UIStackView!
+    @IBOutlet weak var rangedLabel1: UILabel!
+    @IBOutlet weak var rangedLabel2: UILabel!
     
+    @IBOutlet weak var progressView: UIProgressView!
     
     
     var questions: [Question] = [
@@ -62,10 +77,17 @@ class QuestionViewController: UIViewController {
         multipleStackView.isHidden = true
         rangedStackView.isHidden = true
         
-        navigationItem.title = "Question #\(questionIndex+1)"
-        
         let currentQuestion = questions[questionIndex]
+        let currentAnswers = currentQuestion.answers
+        //let totalProgress = Float(questionIndex)} \
+        //Float(questions.count)
         
+        navigationItem.title = "Question #\(questionIndex+1)"
+        questionLabel.text = currentQuestion.text
+        // progressView.setProgress(totalprogress, animated: true)
+        
+
+
         switch currentQuestion.type {
         case.single:
             singleStackView.isHidden = false
@@ -75,7 +97,29 @@ class QuestionViewController: UIViewController {
             rangedStackView.isHidden = false
         
             
-        }
+            }
+    }
+    
+    func updateSingleStack(using answers: [Answer]) {
+        singleStackView.isHidden = false
+        singleButton1.setTitle(answers[0].text, for: .normal)
+        singleButton2.setTitle(answers[1].text, for: .normal)
+        singleButton3.setTitle(answers[2].text, for: .normal)
+        singleButton4.setTitle(answers[3].text, for: .normal)
+        
+    }
+    
+    func updateMultiplStack(using answers: [Answer]) {
+        multipleStackView.isHidden = false
+        multiLabel1.text = answers[0].text
+        multiLabel2.text = answers[1].text
+        multiLabel3.text = answers[2].text
+        multiLabel4.text = answers[3].text
+    }
+    
+    func updateRangedStack(using answers: [Answer]) {
+        rangedLabel1.text = answers.first?.text
+        rangedLabel2.text = answers.last?.text
     }
     
 }
